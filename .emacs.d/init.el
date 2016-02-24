16fe655b75defd757175b7a619aafafa4214955b
Alpha-10000 <abdoul_a@epita.fr>
2016-02-24 17:44:27 +0100

    Updated emacs config files

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,7 +1,7 @@
 (set-language-environment "UTF-8")
 
 ;;Path for plugins
-(add-to-list 'load-path "/home/alpha/.emacs.d/")
+(add-to-list 'load-path "/home/alpha/.emacs.d/plugins")
 
 ;;When using custom method
 (setq custom-file "~/.emacs.d/custom.el")
@@ -14,6 +14,9 @@
 
 (setq c-default-style "bsd") ;;Indent style
 (setq c-basic-offset 2) ;;Tab size
+
+(setq-default make-backup-files nil) ;;Disable temp files
+(setq scroll-step 1) ;;Scroll one line
 
 (global-set-key (kbd "C-c c") 'compile) ;;Make shortcut
 (global-set-key (kbd "C-c g") 'gdb) ;;Debug shortcut
@@ -56,7 +59,7 @@
 
 ;;Autocomplete
 (require 'auto-complete-config)
-(add-to-list 'ac-dictionary-directories "/home/alpha/.emacs.d//ac-dict")
+(add-to-list 'ac-dictionary-directories "/home/alpha/.emacs.d/plugins//ac-dict")
 (ac-config-default)
 
 ;;History in gdb
@@ -65,3 +68,39 @@
      (define-key gud-mode-map (kbd "<up>") 'comint-previous-input)
      (define-key gud-mode-map (kbd "<down>") 'comint-next-input)))
 (put 'erase-buffer 'disabled nil)
+
+;; Tiger
+(add-to-list 'load-path "~/.emacs.d/tiger")
+(autoload 'tiger-mode "tiger" "Load tiger-mode" t)
+(add-to-list 'auto-mode-alist '("\\.ti[gh]$" . tiger-mode))
+
+;; Kernel dev
+(defun c-lineup-arglist-tabs-only (ignored)
+    "Line up argument lists by tabs, not spaces"
+    (let* ((anchor (c-langelem-pos c-syntactic-element))
+	   (column (c-langelem-2nd-pos c-syntactic-element))
+	   (offset (- (1+ column) anchor))
+	   (steps (floor offset c-basic-offset)))
+      (* (max steps 1)
+	 c-basic-offset)))
+
+(add-hook 'c-mode-common-hook
+	  (lambda ()
+	    ;; Add kernel style
+	    (c-add-style
+	     "linux-tabs-only"
+	     '("linux" (c-offsets-alist
+			(arglist-cont-nonempty
+			 c-lineup-gcc-asm-reg
+			 c-lineup-arglist-tabs-only))))))
+
+(add-hook 'c-mode-hook
+	  (lambda ()
+	    (let ((filename (buffer-file-name)))
+	      ;; Enable kernel mode for the appropriate files
+	      (when (and filename
+			 (string-match (expand-file-name "~/project-path")
+				       filename))
+		(setq indent-tabs-mode t)
+		(setq show-trailing-whitespace t)
+		(c-set-style "linux-tabs-only")))))
